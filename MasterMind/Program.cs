1c1ed58d3d54d88a2a5d7f18fe548f5e874421ff
SergeyHaberer <SerdavidHaberer@gmail.com>
2020-04-25 13:33:44 -0500

    work later

---
+++ MasterMind/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MasterMind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PLease pick two colors. Red, Blue, or Yellow.");
+            Console.WriteLine("Enter your guess seperated by a space.");
+
+            string answer = Console.ReadLine().ToLower();
+            string[] userColors = answer.Split(' ');
+
+            Random random = new Random();
+            int first = random.Next(1, 3);
+            int second = random.Next(1, 3);
+
+            string computerColor1 = string.Empty;
+            switch (first)
+            {
+                case 1:
+                    computerColor1 = "red";
+                    break;
+                default:
+                    break;
+            }
+
+            bool isPlaying = false;
+            do
+            {
+
+
+
+            } while (isPlaying);
+        }
+    }
+}
