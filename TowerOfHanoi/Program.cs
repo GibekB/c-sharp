d382796488fb31dcc794141e8facdb455074f87b
SergeyHaberer <SerdavidHaberer@gmail.com>
2020-04-26 21:14:30 -0500

    frustration tower of hanoi

---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TowersOfhanoi //totally not stolen
+{
+    // TowersOfHanoi 
+    class Program
+    {
+        private static Dictionary<string, Stack<int>> board = new Dictionary<string, Stack<int>>();
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(4);
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+            board.Add("A", stack);
+            board.Add("B", new Stack<int>());
+            board.Add("C", new Stack<int>());
+
+            do
+            {
+                Console.Clear();
+                PrintBoard();
+                Console.WriteLine("Choose a tower (A, B, C,) to move from.");
+                string from = Console.ReadLine().ToUpper();
+                Console.WriteLine("Choose to move to tower (A, B, C,).");
+                string to = Console.ReadLine().ToUpper();
+
+                if (IsMoveValid(from, to))
+                {
+                    board[to].Push(board[from].Pop());
+                }
+                 // moving disks(stacks) from pole to pole(boards)
+                else
+                {
+                    Console.WriteLine("Wrong move buster.");
+                    Console.WriteLine("Press (SPACE) for a second chance.");
+                    Console.ReadKey();
+                }
+
+            } while (!CheckWin());
+
+            Console.Clear();
+            PrintBoard();
+            Console.WriteLine("Well i'll be darned you're as smart as a monkey");
+            Console.ReadKey();
+        }
+
+        private static bool IsMoveValid(string from, string to)
+        {
+            if (board[from].Count == 0)
+            {
+                // no disks(stacks) present
+                return false;
+            }
+            else if (to == from)
+            {
+                // can't move a peg from and to the same pole(board)
+                return false;
+            }
+            // check that the destination pole(board) is not empty before using Peek, otherwise it will throw an error
+            else if (board[to].Count != 0)
+            {
+                // A bigger disk (stack) cant be on top of a smaller one (presented by numbers) 
+                if (board[to].Peek() > board[from].Peek())
+                {
+                    return false;
+                }
+
+                
+                return true;
+            }
+            else 
+            {
+                return true;
+            } // this checks out valid
+        }
+
+        private static bool CheckWin()
+        {
+            // You need to move all the disks(stacks 1,2,3,4) from pole A (baord A) to pole C (board C) from greates (4) to smallest (1)
+            if (board["C"].Count == 4)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void PrintBoard()
+        {
+            foreach (var item in board)
+            {
+                Console.Write($"\n{item.Key}: ");
+                var numbers = item.Value.ToArray();
+                // updates the board
+                for (int i = numbers.Length; i > 0; i--)
+                {
+                    Console.Write(numbers[i - 1] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TowerOfHanoi/README.md
@@ -0,0 +1,7 @@
+Towers of Hanoi by Sergey
+
+I'm going to have boards in my dictionary that represent the poles. I then have ints stacks that represent the rings 
+or disks that have to be moved from board to board. I then have if and if else letting the player know what to do and 
+if he made a mistake alerting them when they win. Using a do while checkwin to check if the player wins. the if and if
+else outside of do while is the method for true and false for inaccurate inputs. and my final method refreshes the 
+board every turn.
